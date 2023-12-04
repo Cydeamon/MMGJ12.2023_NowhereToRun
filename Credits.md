ddebba8f0428eb832fe688a73a01a5024200ab20
Cydeamon <loknhess@gmail.com>
2023-12-04 03:12:24 +0300

    Submenus

---
+++ Credits.md
@@ -0,0 +1,5 @@
+# Credits
+
+* ["**Red-Blood Pain Palette**"](https://lospec.com/palette-list/red-blood-pain)
+ by [**ahmed**](https://lospec.com/gxndy)
+
--- Sources/Menu.cs
+++ Sources/Menu.cs
@@ -13,6 +13,12 @@ public partial class Menu : Control
 
     [Signal] public delegate void ExitEventHandler();
 
+
+    /***************************************************************************/
+    /******************************** Properties *******************************/
+
+    private Control ActiveSubmenu = null;
+
     /***************************************************************************/
     /********************************* Methods *********************************/
 
@@ -21,25 +27,41 @@ public partial class Menu : Control
     {
         // Connect signals
         GetNode<TextureButton>("MenuOptions/StartGame").Pressed += OnStartGamePressed;
-        GetNode<TextureButton>("MenuOptions/HighScores").Pressed += OnHighScoresPressed;
-        GetNode<TextureButton>("MenuOptions/Credits").Pressed += OnCreditsPressed;
         GetNode<TextureButton>("MenuOptions/Exit").Pressed += OnExitPressed;
+
+        // Submenus activation
+        foreach (TextureButton option in GetNode<VBoxContainer>("MenuOptions").GetChildren())
+        {
+            if (option.Name != "StartGame" && option.Name != "Exit")
+                option.Pressed += ActivateSubmenu;
+        }
+
+        // Submenus "Go Back" options
+        foreach (Control submenu in GetNode("Submenus").GetChildren())
+            submenu.GetNode<TextureButton>("GoBack").Pressed += GoBack;
     }
 
+    public void ActivateSubmenu()
+    {
+        if (GetViewport().GuiGetFocusOwner() is TextureButton menuOption)
+        {
+            Control submenu = GetNode<Control>("Submenus/" + menuOption.Name);
+            ActiveSubmenu = submenu;
+            submenu.Show();
+            GetNode<Control>("MenuOptions").Hide();
+        }
+    }
+
+    public void GoBack()
+    {
+        ActiveSubmenu.Hide();
+        ActiveSubmenu = null;
+        GetNode<Control>("MenuOptions").Show();
+    }
 
     public void OnStartGamePressed()
     {
         EmitSignal("StartGame");
-    }
-
-    public void OnHighScoresPressed()
-    {
-        EmitSignal("HighScores");
-    }
-
-    public void OnCreditsPressed()
-    {
-        EmitSignal("Credits");
     }
 
     public void OnExitPressed()
